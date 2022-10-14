91c810cff8979c44c007f1e5e8823445511f9aa7
Aminarh22 <laurel.lorel@gmail.com>
2022-10-14 13:44:32 -0700

    second

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-#incude <unistd.h>
+#include <unistd.h>
 /**
- * main Entry
- * Return: 1
- */
-int main (void)
+ *  *main-program entry point.
+ *   *
+ *    *Return: 1 on success.
+ *     */
+int main(void)
 {
-	write(1, "and the piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
