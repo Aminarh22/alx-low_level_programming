b8fa649f995ca2b20a64d344b97ff463289a3328
Aminarh22 <laurel.lorel@gmail.com>
2022-10-13 20:23:17 -0700

    first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#incude <unistd.h>
+/**
+ * main Entry
+ * Return: 1
+ */
+int main (void)
+{
+	write(1, "and the piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
