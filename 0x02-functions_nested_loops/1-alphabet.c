b8e576205dd9125f3c84dab9281654638b7bfa05
Aminarh22 <laurel.lorel@gmail.com>
2022-10-19 15:59:56 -0700

    looops fixed 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* print_alphabet -> prints the lowercase alpabets
+* prin_alphabet -> prints the lowercase alphabets
 */
 
 void print_alphabet(void)
 {
 	int j;
 
-	for (j = 'a'; j <= 'z'; j++)
+	for (j = 'a'; j <= 'z; j++)
 	{
 		_putchar(j);
 	}
-	_putchar('\n');
+	_putchar(j);
 }
